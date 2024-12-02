7d86d55b6489b2e172d847943773ada7db6757b3
FabioGigante <s328890@studenti.polito.it>
2024-12-02 17:58:58 +0100

    Add all files to make gxgp work

--- d3584.py
+++ d3584.py
@@ -1,7 +1,3 @@
-# Copyright © 2024 Giovanni Squillero <giovanni.squillero@polito.it>
-# https://github.com/squillero/computational-intelligence
-# Free under certain conditions — see the license for details.
-
 import numpy as np
 
 
